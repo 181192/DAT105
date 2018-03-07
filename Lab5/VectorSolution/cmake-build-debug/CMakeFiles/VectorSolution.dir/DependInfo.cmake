96b577d454fed209a22f553bc077bd0055aa4aff
181192 <181192@stud.hvl.no>
2018-03-07 21:28:19 +0100

    Refactor

---
+++ Lab5/VectorSolution/cmake-build-debug/CMakeFiles/VectorSolution.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/k/Data/Utvikling/Skole/DAT105/Lab5/VectorSolution/main.cpp" "/media/k/Data/Utvikling/Skole/DAT105/Lab5/VectorSolution/cmake-build-debug/CMakeFiles/VectorSolution.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab5/VectorSolution/cmake-build-debug/CMakeFiles/VectorSolution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorSolution.dir/main.cpp.o"
+  "VectorSolution.pdb"
+  "VectorSolution"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorSolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
