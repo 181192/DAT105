ef8b3fc37b4b67de755109b16b825884b5bdd37a
181192 <181192@stud.hvl.no>
2018-03-07 21:17:45 +0100

    Implemented vectorToString

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -27,11 +27,9 @@ int vectorSum1(vector<int> v) {
 
 int vectorSum2(vector<int> v) {
     int sum = 0;
-    for_each(begin(v), end(v),
-             [&](int e) {
-                 sum += e;
-             }
-    );
+    for_each(begin(v), end(v), [&](int e) {
+        sum += e;
+    });
     return sum;
 };
 
@@ -47,38 +45,44 @@ void sortVector1(vector<T> &v) {
 
 template<class T>
 void sortVector2(vector<T> &v) {
-    sort(v.begin(), v.end(), [](const T &a, const T &b) {
+    sort(v.begin(), v.end(), [](T &a, T &b) {
         return a > b;
     });
 }
 
-string vectorToString1(vector<int> v) {
-    std::stringstream ss;
-    for (int i = 0; i < v.size(); ++i) {
-        if (i != 0)
-            ss << ",";
-        ss << v[i];
+template<class T>
+string vectorToString1(vector<T> &v) {
+    stringstream ss;
+    for (T e : v) {
+        ss << e << " ";
     }
     return ss.str();
 }
 
-auto vectorToString2 = [](vector<int> v) {
-    // TODO Not implemented
-    return "Eyy";
-};
+template<class T>
+string vectorToString2(vector<T> &v) {
+    stringstream ss;
+    for_each(v.begin(), v.end(), [&](T &e) {
+        ss << e << " ";
+    });
+}
 
 int main() {
     vector<int> v = {1, 9, 7, 3, 5, 6, 2, 8, 4};
+
     printHelloWorld1();
     printHelloWorld2();
+
     cout << "Sum = " << calculateSum1(5, 8) << endl;
     cout << "Sum = " << calculateSum2(5, 8) << endl;
     cout << "Sum = " << vectorSum1(v) << endl;
     cout << "Sum = " << vectorSum2(v) << endl;
+
     vector<int> vectorCopy1{v};
     cout << vectorToString1(vectorCopy1) << endl;
     sortVector1(vectorCopy1);
     cout << vectorToString1(vectorCopy1) << endl;
+
     vector<int> vectorCopy2{v};
     cout << vectorToString2(vectorCopy2) << endl;
     sortVector2(vectorCopy2);
