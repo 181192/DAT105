8375d642bac86e4c7a2c147faccfc66f755d59a3
181192 <181192@stud.hvl.no>
2018-05-15 20:09:46 +0200

    Implemented guess method

--- QtLab02/hangmanlogic.cpp
+++ QtLab02/hangmanlogic.cpp
@@ -13,7 +13,8 @@ void HangmanLogic::startNewGame(QString hiddenString)
     emit resetUi();
 
     hiddenMsg = hiddenString.toUpper();
-    hiddenMsgScrambled = hiddenMsg.replace(QRegExp("[A-ZØÆÅ]"), "_");
+    hiddenMsgScrambled = hiddenMsg;
+    hiddenMsgScrambled.replace(QRegExp("[A-ZØÆÅ]"), "_");
 
     attemptNumber = 0;
 
@@ -30,5 +31,41 @@ void HangmanLogic::restartGame()
 
 void HangmanLogic::guess(QChar c)
 {
+
+    if (attemptNumber == MAX_ATTEMPS)
+        return;
+
+    if (QString(c)[0].isLetter() == false)
+        return;
+
+    QChar inputChar = QString(c).toUpper()[0];
+
+    if (hiddenMsg.toLatin1().contains(inputChar.toLatin1())) {
+        emit setButtonGreen(c);
+
+        QString searchString = hiddenMsg;
+        int index = 0;
+        while( (index = searchString.indexOf(inputChar, index)) != -1 )
+            hiddenMsgScrambled.replace(index++, 1, inputChar);
+
+        emit updateHiddenMessage(hiddenMsgScrambled);
+
+        if (hiddenMsgScrambled.contains("_") == false) {
+            emit updateStatusMessage("You win!");
+            emit gameOver();
+            attemptNumber = MAX_ATTEMPS;
+        }
+    } else {
+        emit setButtonRed(c);
+        attemptNumber++;
+        emit updateAttemptsLeft(attemptNumber);
+
+        if (attemptNumber == MAX_ATTEMPS) {
+            emit updateStatusMessage("You lose!");
+            emit gameOver();
+        } else {
+            emit updateStatusMessage(QString("Attempts left: %1").arg(MAX_ATTEMPS-attemptNumber));
+        }
+    }
 
 }
